0edcdff6b289df9784f12ebfbf173568b1e332b9
Damien MAILHEBIAU <damienmailhebiau@gmail.com>
2022-10-15 00:17:05 +0200

    Add animations

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import {SharedModule} from "../shared/shared.module";
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -14,6 +15,8 @@ import {HttpClientModule} from "@angular/common/http";
     SharedModule,
     RouterModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+
   ],
   exports: [
     HeaderComponent
--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,11 +1,32 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Comment} from "../../../core/models/comment.model";
-import {FormBuilder, FormControl, Validator, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        transform: 'scale(1.05)',
+        'background-color': `rgb(201,157,242)`,
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ]),
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
 
@@ -13,11 +34,15 @@ export class CommentsComponent implements OnInit {
   @Output() newComment = new EventEmitter<string>();
 
   commentCtrl!: FormControl;
+  animationState: { [key: number]: 'default' | 'active' } = {};
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
+    for (let index in this.comments) {
+      this.animationState[index] = 'default';
+    }
   }
 
   onLeaveComment() {
@@ -27,4 +52,13 @@ export class CommentsComponent implements OnInit {
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
   }
+
+  onListItemMouseEnter(index: number) {
+    this.animationState[index] = 'active';
+  }
+
+  onListItemMouseLeave(index: number) {
+    this.animationState[index] = 'default';
+  }
+
 }
